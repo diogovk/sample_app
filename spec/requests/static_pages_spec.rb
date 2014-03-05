e5e17d61adb7db0bb70813a27071e31db300fc0d
Diogo Kersting <diogovk@gmail.com>
2014-03-05 18:06:25 -0300

    learning to use layouts and basic template

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,14 +6,26 @@ describe "StaticPages" do
       visit '/static_pages/home'
       expect(page).to have_content('Sample App')
     end
+    it "should have right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Ruby | Home")
+    end
   end
   describe "Help Page" do
+    it "should have right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Ruby | Help")
+    end
     it "should have 'Help'" do
       visit '/static_pages/help'
       expect(page).to have_content('Help')
     end
   end
   describe "About Page" do
+    it "should have right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("Ruby | About")
+    end
     it "shoud have the content 'About Us'" do
       visit '/static_pages/about'
       expect(page).to have_content('About Us')
