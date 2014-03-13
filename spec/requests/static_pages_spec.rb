2a61ccdf960ee7468a22bdfc29b8610966423064
Diogo Kersting <diogovk@gmail.com>
2014-03-13 08:55:26 -0300

    changes in tests, "users new view"

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,34 +2,40 @@ require 'spec_helper'
 
 describe "Static pages" do
   subject { page }
+
   describe "Home page" do
     before { visit root_path }
-
-    it { should have_content('Sample App') }
-    
+    it { should have_selector('h1', text: 'Sample App') }
     it { should have_title("Ruby on Rails Tutorial Sample App") }
-
     it { should_not have_title('| Home') }
   end
 
-  describe "Help page" do
-    before { visit help_path } 
-    it { should have_content('Help') } 
+  shared_examples_for "non-home static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title("Ruby on Rails Tutorial Sample App | #{page_title}") }
+  end
 
-    it { should have_title("Ruby on Rails Tutorial Sample App | Help") }
+  describe "Help page" do
+    before { visit help_path }
+
+    let(:heading) { 'Help' }
+    let(:page_title) { 'Help' }
+    it_should_behave_like "non-home static pages"
   end
 
   describe "About page" do
     before { visit about_path }
-    it { should  have_content('About Us') } 
 
-    it { should have_title("Ruby on Rails Tutorial Sample App | About Us") }
+    let(:heading) { 'About Us' }
+    let(:page_title) { 'About Us' }
+    it_should_behave_like "non-home static pages"
   end
 
   describe "Contact page" do
     before { visit contact_path }
-    it { should have_content('Contact') }
 
-    it { should have_title("Ruby on Rails Tutorial Sample App | Contact") }
+    let(:heading) { 'Contact' }
+    let(:page_title) { 'Contact' }
+    it_should_behave_like "non-home static pages"
   end
 end
