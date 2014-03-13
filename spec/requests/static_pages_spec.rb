3d1dc0434035a7c754daf4da7b99927c367c7f27
Diogo Kersting <diogovk@gmail.com>
2014-03-13 13:08:22 -0300

    end of chapter 5

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,18 +1,19 @@
 require 'spec_helper'
+include ApplicationHelper
 
 describe "Static pages" do
   subject { page }
 
-  describe "Home page" do
-    before { visit root_path }
-    it { should have_selector('h1', text: 'Sample App') }
-    it { should have_title("Ruby on Rails Tutorial Sample App") }
-    it { should_not have_title('| Home') }
+  shared_examples_for "all static pages" do
+    it { should have_selector('h1', text: heading) }
+    it { should have_title(full_title(page_title)) }
   end
 
-  shared_examples_for "non-home static pages" do
-    it { should have_selector('h1', text: heading) }
-    it { should have_title("Ruby on Rails Tutorial Sample App | #{page_title}") }
+  describe "Home page" do
+    before { visit root_path }
+    let(:heading) { 'Sample App' }
+    let(:page_title) { "" }
+    it_should_behave_like "all static pages"
   end
 
   describe "Help page" do
@@ -20,7 +21,7 @@ describe "Static pages" do
 
     let(:heading) { 'Help' }
     let(:page_title) { 'Help' }
-    it_should_behave_like "non-home static pages"
+    it_should_behave_like "all static pages"
   end
 
   describe "About page" do
@@ -28,7 +29,7 @@ describe "Static pages" do
 
     let(:heading) { 'About Us' }
     let(:page_title) { 'About Us' }
-    it_should_behave_like "non-home static pages"
+    it_should_behave_like "all static pages"
   end
 
   describe "Contact page" do
@@ -37,7 +38,7 @@ describe "Static pages" do
     let(:heading) { 'Contact' }
     let(:page_title) { 'Contact' }
 
-    it_should_behave_like "non-home static pages"
+    it_should_behave_like "all static pages"
   end
 
   it "should have the correct links in the layout" do
