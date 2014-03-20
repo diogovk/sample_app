bb056e635b1a6909ee2f51a9ae0bb06f5251dc98
Diogo Kersting <diogovk@gmail.com>
2014-03-20 18:19:50 -0300

    show and test profile page

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -1,10 +1,18 @@
 require 'spec_helper'
 
+include ApplicationHelper
+
 describe "User Pages" do
   subject { page }
   describe "signup page" do
     before { visit signup_path }
     it { should have_content('Sign up') }
-    it { should have_title('Ruby on Rails Tutorial Sample App | Sign up') }
+    it { should have_title(full_title('Sign up')) }
+  end
+  describe "profile page" do
+    before { visit user_path(user) }
+
+    it { should have_content(user.name) }
+    it { should have_title(user.name) }
   end
 end
