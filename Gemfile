51f131757464d196b1b020608858104da4421571
Diogo Kersting <diogovk@gmail.com>
2014-03-24 12:57:13 -0300

    User FactoryGirl, gravatar

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ end
 
 group :test do
   gem 'capybara'
+  gem 'factory_girl_rails', '4.2.1'
   gem 'rb-fsevent', '0.9.3', :require => false
   gem 'growl', '1.0.3'
 end
---
+++ spec/factories.rb
@@ -0,0 +1,10 @@
+
+FactoryGirl.define do
+  factory :user do
+    name "Michael Hart"
+    email "email@example.com"
+    password "foobar"
+    password_confirmation "foobar"
+  end
+end
+
